03b7d9e1f52854be756d4ec05f22b2e1403f0c63
raihaanshaarif <codexbd@gmail.com>
2021-10-27 19:21:39 +0600

    first'

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pfshu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Connected to Database');
+
+
+    } finally {
+        await client.close();
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Running Genius Server')
+})
+
+
+app.listen(port, () => {
+    console.log('running genius server on', port);
+})
